52471f9842385ad933e822fbdbaa01e2ca737f66
Burningstone91 <dimitri.steiner.gl@gmail.com>
2019-07-09 21:20:42 +0200

    updated HA to 0.95.4

--- appdaemon/configuration/apps/test.py
+++ appdaemon/configuration/apps/test.py
@@ -4,14 +4,18 @@ from appbase import AppBase
 class TestClass(AppBase):
 
     def configure(self):
-        self.handle = self.run_daily(self.pump_on, self.parse_time("21:13:00"))
-        self.listen_state(self.pump_on, 'switch.schalter_entfeuchter', new='on')
+        # self.handle = self.run_in(self.pump_on, 5, entity_id="4", action="5")
+        self.pump_on({"entity_id": "switch.schalter_entfeuchter", "action": 'off'})
 
-    def pump_on(self, *args, **kwargs):
-        self.log("module: __module__, function: __function__, MSG: Paused registered")
-        for item, value in kwargs.items():
-            self.log(item)
-            self.log(value)
-        for arg in args:
-            self.log(arg)
+    def pump_on(self, kwargs):
 
+        action = kwargs.get('action')
+        entity_id = kwargs.get('entity_id')
+        state = self.get_state(kwargs['entity_id'])
+        if state != action:
+            if action == 'on':
+                self.turn_on(entity_id)
+            else:
+                self.turn_off(entity_id)
+            self.log('hier fehler')
+            self.run_in(self.pump_on, 5, entity_id=entity_id, action=action)
